7ba001f447d4af1779a71672974303b46170278f
Parthivi Thakore <119952219+parthivithakorecode@users.noreply.github.com>
2023-03-16 21:09:41 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+import react from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import girlWorkout from './assets/girlWorkout.jpg';
+import pushup from './assets/pushup.jpg';
+import squat from './assets/squat.jpg';
+import WorkoutCard from './components/WorkoutCard';
+const workouts = [
+  {
+    title:'Short, sweet, sweat',
+    time: 3,
+    image: girlWorkout,
+    //onPress: () =>{ console.log("working"); setWorkoutType('3min') },
+  },
+  {
+    title:'Full body burner',
+    time: 5,
+    image: pushup,
+    //onPress: () =>{ setWorkoutType('5min') },
+  },
+  {
+    title:'Daily intense workout',
+    time: 8,
+    image: squat, 
+    //onPress: () =>{ setWorkoutType('8min') },
+  },
+]
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Lets Workout</Text>
+      <Text>with home equipments</Text>
+      <Text>Get Started in just a minute 💪</Text>
+      {
+      workouts.map(( workout) => { const {title, time, image} = workout;
+      return WorkoutCard(title, time, image);
+      })}
     </View>
   );
 }
